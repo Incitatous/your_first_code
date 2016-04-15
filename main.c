4cf5bb757ba7b90a971a758a4a56972a79143793
Incitatous <dcianberlin@gmail.com>
2016-04-15 20:23:54 +0200

    Update main.c
    It should now work properly :)

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+
+//Hello Holberton! I am very excited about this opportunity :)
 int main()
 {
   int i;
   for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
